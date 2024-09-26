1b9d35f5e64162ca2362999ac5411c8bc105360f
daniele-farriciello <daniele.farriciello.03@gmail.com>
2024-09-26 16:49:11 +0100

    fix: model user

--- model/user.go
+++ model/user.go
@@ -3,21 +3,21 @@ package model
 import "time"
 
 type User struct {
-	ID        uint       `db:"id" json:"id"`
-	FirstName string     `db:"firstName" json:"firstName"`
-	LastName  string     `db:"lastName" json:"lastName"`
-	Email     string     `db:"email" json:"email"`
-	Password  string     `db:"password" json:"password"`
-	TypeId    uint       `db:"typeId" json:"typeId"`
-	CreatedAt time.Time  `db:"createdAt" json:"createdAt"`
-	UpdatedAt time.Time  `db:"updatedAt" json:"updatedAt"`
-	DeletedAt *time.Time `db:"deletedAt" json:"deletedAt"`
+	ID         uint       `db:"id" json:"id"`
+	FirstName  string     `db:"first_name" json:"first_name"`
+	LastName   string     `db:"last_name" json:"last_name"`
+	Email      string     `db:"email" json:"email"`
+	Password   string     `db:"password" json:"password"`
+	TypeUserId uint       `db:"type_user_id" json:"type_user_id"`
+	CreatedAt  time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt  time.Time  `db:"updated_at" json:"updated_at"`
+	DeletedAt  *time.Time `db:"deleted_at" json:"deleted_at"`
 }
 
 type RegistrationPayload struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	TypeId    uint   `json:"typeId"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	Email      string `json:"email"`
+	TypeUserId uint   `json:"type_user_id"`
+	Password   string `json:"password"`
 }
