744e8f9f8db02bfa437ef05ea1558256cea11baa
daniele-farriciello <daniele.farriciello.03@gmail.com>
2024-09-26 16:50:22 +0100

    db table

--- load_db/1.sql
+++ load_db/1.sql
@@ -1,28 +1,20 @@
 CREATE DATABASE IF NOT EXISTS `act-db`;
 USE `act-db`;
 
-CREATE TABLE IF NOT EXISTS `type_user` (
-    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `typeOfUser` VARCHAR(255) NOT NULL,
-    `companyAllowed` INT NOT NULL
+CREATE TABLE IF NOT EXISTS type_user (
+    id SERIAL PRIMARY KEY,
+    type_of_user VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `companies` (
-    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `companyName` VARCHAR(255) NOT NULL,
-    `userId` INT UNSIGNED NOT NULL,
-    CONSTRAINT `fk_company_user` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `firstName` VARCHAR(255) NOT NULL,
-    `lastName` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `typeUserId` INT UNSIGNED NOT NULL,
-    `createdAt` DATETIME NOT NULL,
-    `updatedAt` DATETIME NOT NULL,
-    `deletedAt` DATETIME,
-    CONSTRAINT `fk_user_type_user` FOREIGN KEY (`typeUserId`) REFERENCES `type_user` (`id`)
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    type_user_id INTEGER NOT NULL,  
+    created_at TIMESTAMP NOT NULL, 
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP,        
+    CONSTRAINT fk_user_type_user FOREIGN KEY (type_user_id) REFERENCES type_user (id)
 );
