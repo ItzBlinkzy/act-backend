043d62f92da8a27a6f628307aea7d9a7774a85af
daniele-farriciello <daniele.farriciello.03@gmail.com>
2024-10-17 14:46:06 +0100

    feat: tables added

--- load_db/1.sql
+++ load_db/1.sql
@@ -3,7 +3,7 @@ USE `act-db`;
 
 CREATE TABLE IF NOT EXISTS type_user (
     id SERIAL PRIMARY KEY,
-    type_of_user VARCHAR(255) NOT NULL
+    type_of_user VARCHAR(255) NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS users (
@@ -25,10 +25,12 @@ CREATE TABLE IF NOT EXISTS bought_stocks (
     ticker VARCHAR(255) NOT NULL,
     quantity_owned INTEGER,
     quantity_sold INTEGER,
+    client_id INTEGER,
     created_at TIMESTAMP NOT NULL, 
     updated_at TIMESTAMP NOT NULL,
     deleted_at TIMESTAMP,        
-    CONSTRAINT fk_user_bought_stocks FOREIGN KEY (user_id) REFERENCES users (id)
+    CONSTRAINT fk_user_bought_stocks FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_client_bought_stocks FOREIGN KEY (client_id) REFERENCES clients (id)
 );
 
 CREATE TABLE IF NOT EXISTS reviews (
@@ -40,4 +42,23 @@ CREATE TABLE IF NOT EXISTS reviews (
     updated_at TIMESTAMP NOT NULL,
     deleted_at TIMESTAMP,   
     CONSTRAINT fk_review_user FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS client_manager_association (
+    id SERIAL PRIMARY KEY,
+    manager_id INTEGER NOT NULL,
+    client_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL, 
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP,   
+    CONSTRAINT fk_client_user FOREIGN KEY (client_id) REFERENCES clients (id),
+    CONSTRAINT fk_manager_user FOREIGN KEY (manager_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS clients (
+    id SERIAL PRIMARY KEY,
+    company_name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL, 
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP
 );
