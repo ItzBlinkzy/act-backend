d3cb02d489b6cb882c0bbf6db3ed75b6fcd57733
daniele-farriciello <daniele.farriciello.03@gmail.com>
2024-10-17 14:46:30 +0100

    feat: stock model updated

--- model/stock.go
+++ model/stock.go
@@ -8,6 +8,7 @@ type Stock struct {
 	Ticker        string     `db:"ticker" json:"ticker"`
 	QuantityOwned *int       `db:"quantity_owned" json:"quantity_owned"`
 	QuantitySold  *int       `db:"quantity_sold" json:"quantity_sold"`
+	ClientId      *uint      `db:"client_id" json:"client_id"`
 	CreatedAt     time.Time  `db:"created_at" json:"created_at"`
 	UpdatedAt     time.Time  `db:"updated_at" json:"updated_at"`
 	DeletedAt     *time.Time `db:"deleted_at" json:"deleted_at"`
@@ -15,6 +16,7 @@ type Stock struct {
 
 type StockBuy struct {
 	UserId         uint   `db:"user_id" json:"user_id"`
+	Clientid       *uint  `db:"client_id" json:"client_id"`
 	Ticker         string `json:"ticker"`
 	BuyingQuantity int    `json:"buying_quantity"`
 }
@@ -23,4 +25,5 @@ type StockUpdate struct {
 	Ticker          string `json:"ticker"`
 	BuyingQuantity  int    `json:"buying_quantity"`
 	SellingQuantity int    `json:"selling_quantity"`
+	ClientId        *uint  `db:"client_id" json:"client_id"`
 }
