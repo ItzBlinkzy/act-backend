fcc88cb3afc6ad24f3937d9e4de2e36a765cc28b
daniele-farriciello <daniele.farriciello.03@gmail.com>
2024-10-24 19:21:43 +0100

    feat: logs table added

--- load_db/1.sql
+++ load_db/1.sql
@@ -33,6 +33,19 @@ CREATE TABLE IF NOT EXISTS bought_stocks (
     CONSTRAINT fk_client_bought_stocks FOREIGN KEY (client_id) REFERENCES clients (id)
 );
 
+CREATE TABLE IF NOT EXISTS logs_bought_stocks (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    bought_stock_id INTEGER NOT NULL,
+    quantity_bought INTEGER DEFAULT NULL,
+    quantity_sold INTEGER DEFAULT NULL,
+    client_id INTEGER,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    CONSTRAINT fk_user_bought_stocks FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_logs_bought_stocks FOREIGN KEY (bought_stock_id) REFERENCES bought_stocks (id),
+    CONSTRAINT fk_logs_clients_stocks FOREIGN KEY (client_id) REFERENCES clients (id)
+);
+
 CREATE TABLE IF NOT EXISTS reviews (
     id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
