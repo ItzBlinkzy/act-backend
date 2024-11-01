8f79d5f96877d91f0810a38e34c1424aa289b1b9
Blinkzy <68260779+ItzBlinkzy@users.noreply.github.com>
2024-11-01 16:15:20 +0000

    fix: redudant package use

--- model/login.go
+++ model/login.go
@@ -1,11 +1,11 @@
 package model
 
 type LoginPayload struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+    Email    string `json:"email"`
+    Password string `json:"password"`
 }
 
 type LoginResponse struct {
     Token  string `json:"token"`
-    ClientManagerAssociations []model.ClientManagerAssociation `json:"client_manager_associations"`
+    ClientManagerAssociations []ClientManagerAssociation `json:"client_manager_associations"`
 }
